cd9c0cc36adb738312c9d1ba0615882d76fa0db3
HawkinsWeb <128600888+crmhawkins@users.noreply.github.com>
2026-04-30 10:36:19 +0200

    fix: use $fillable array instead of PHP attributes for mass assignment

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,18 +6,17 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $fillable = ['name', 'email', 'password'];
+    protected $hidden = ['password', 'remember_token'];
 
     public function canAccessPanel(Panel $panel): bool
     {
